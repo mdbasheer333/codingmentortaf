3db5a4590faf949c2de3c178d55d10a59d29c86c
mdbasheer333 <mdbasheer333@gmail.com>
2022-05-12 10:04:04 +0530

    fix of chrome

--- src/main/java/com/codingmentor/LoginPage.java
+++ src/main/java/com/codingmentor/LoginPage.java
@@ -4,6 +4,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class LoginPage {
 
@@ -36,6 +38,8 @@ public class LoginPage {
 	}
 
 	public void logoutApp() {
+		WebDriverWait wait=new WebDriverWait(driver, 20);
+		wait.until(ExpectedConditions.visibilityOf(lnkLogout));
 		lnkLogout.click();
 	}
 
