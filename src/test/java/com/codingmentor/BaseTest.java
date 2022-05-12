374f7ac9a13bff13722c3dbcdfaaef090b906325
mdbasheer333 <mdbasheer333@gmail.com>
2022-05-12 09:09:11 +0530

    fix of chrome

--- src/test/java/com/codingmentor/BaseTest.java
+++ src/test/java/com/codingmentor/BaseTest.java
@@ -24,7 +24,7 @@ public class BaseTest {
 		case "chrome":
 		case "gc":
 			WebDriverManager.chromiumdriver().setup();
-			System.setProperty("webdriver.chrome.driver","/usr/local/bin/chromedriver");
+			//System.setProperty("webdriver.chrome.driver","/usr/local/bin/chromedriver");
 			ChromeOptions options = new ChromeOptions();
 			options.addArguments("–no-sandbox");
 			options.addArguments("–disable-dev-shm-usage");
